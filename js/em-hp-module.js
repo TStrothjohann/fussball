346b86627f708e19272732cfd9c5bf6c3ce068c0
TStrothjohann <thomas@strothjohann.net>
2016-05-24 12:12:48 +0200

    added basic FE

---
+++ js/em-hp-module.js
@@ -0,0 +1,36 @@
+var HTTPrequest = new XMLHttpRequest();
+HTTPrequest.open('GET', 'http://phpscripts.zeit.de/fb_fwm/fb_mbl/feed/s2015/md3/dpa/onl1.json', true);
+var data = {};
+var markup = document.getElementById('em-ticker').innerHTML;
+
+HTTPrequest.onload = function() {
+  if (HTTPrequest.status >= 200 && HTTPrequest.status < 400) {
+    // Success!
+    data = JSON.parse(HTTPrequest.responseText);
+    if(data !== {} && markup){
+      document.getElementById('em-ticker').innerHTML = findAndReplaceHandleBars(markup, data);
+    }
+  } else {
+    // We reached our target server, but it returned an error
+
+  }
+};
+
+HTTPrequest.onerror = function() {
+  console.log("There was a connection error of some sort");
+};
+
+HTTPrequest.send();
+
+
+//Templating
+function findAndReplaceHandleBars(tickerMarkup, liveData){
+  var regex = /{{(.*?)}}/;
+  while(regex.test(tickerMarkup)){
+    var match = regex.exec(tickerMarkup);
+    var nextHandles = match[0];
+    var objectValue = eval( "liveData." + match[1] );
+    tickerMarkup = tickerMarkup.replace(nextHandles, objectValue);
+  }
+  return tickerMarkup.replace(/[\r\t\n]/g, '');
+}
